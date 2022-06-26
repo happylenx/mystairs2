a5ba2c0c4f0a9cd8df99acf9858848baf71d2cc5
happylenx <happylenx@users.noreply.github.com>
2022-06-26 19:20:05 +0800

    first commit

---
+++ configure.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+rm -rf /tmp/xray
+
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+  "log": {
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "VLESS",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "alterId": 0
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
